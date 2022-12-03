62c01b829eb40ea189ade5782741c7a89d85c6ed
Takatada Yoshima <yoshima@shiimaxx.com>
2022-12-03 08:44:17 +0000

    Update

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ mkdir -p ~/work/github.com/shiimaxx
 cd $_
 unzip ${tempdir}/master.zip -d .
 mv dotfiles-master dotfiles
+
+rm -rf ${tempdir}
+
+ln -fs ~/work/github.com/shiimaxx/dotfiles/gitconfig ~/.gitconfig
+ln -fs ~/work/github.com/shiimaxx/dotfiles/gitignore ~/.gitignore
+ln -fs ~/work/github.com/shiimaxx/dotfiles/tmux.conf ~/tmux.conf
+ln -fs ~/work/github.com/shiimaxx/dotfiles/vimrc ~/.vimrc
+ln -fs ~/work/github.com/shiimaxx/dotfiles/zsrc ~/.zsrc
+
+mkdir -p ~/config
+ln -fs ~/work/github.com/shiimaxx/dotfiles/config/starship.toml ~/config/starship.toml
