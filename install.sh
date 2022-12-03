9baf4522b4bf5d12e866a5f027dd6c59cb43bf39
Takatada Yoshima <yoshima@fulldrive.jp>
2022-12-03 20:18:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ ln -fs ~/work/github.com/shiimaxx/dotfiles/gitconfig ~/.gitconfig
 ln -fs ~/work/github.com/shiimaxx/dotfiles/gitignore ~/.gitignore
 ln -fs ~/work/github.com/shiimaxx/dotfiles/tmux.conf ~/.tmux.conf
 ln -fs ~/work/github.com/shiimaxx/dotfiles/vimrc ~/.vimrc
-ln -fs ~/work/github.com/shiimaxx/dotfiles/zsrc ~/.zsrc
+ln -fs ~/work/github.com/shiimaxx/dotfiles/zshrc ~/.zshrc
 
 mkdir -p ~/config
 ln -fs ~/work/github.com/shiimaxx/dotfiles/config/starship.toml ~/config/starship.toml
