5bca10392038b4d2a76b158b8c8863715ff9b246
Takatada Yoshima <yoshima@fulldrive.jp>
2022-12-07 17:03:42 +0900

    Update

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,5 @@
 local wezterm = require 'wezterm'
 
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
-
 return {
   color_scheme = "Eldorado dark (terminal.sexy)",
 
